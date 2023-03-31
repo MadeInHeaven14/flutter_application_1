bbfd384fb6101afd12447f699c00d928ec48a659
ZalHame <zalialovmurat@yandex.ru>
2023-03-31 16:32:13 +0300

    добавил поиск

--- lib/dbPage.dart
+++ lib/dbPage.dart
@@ -11,12 +11,61 @@ class dbPage extends StatefulWidget {
 }
 
 dynamic documentFirebase;
+bool tittleAppBar = false;
+TextEditingController searchController =TextEditingController();
+String title = "Список дел";
+
 
 class _dbPageState extends State<dbPage> {
 
+onItemSearch(String value){
+  setState(() {
+    
+  });
+
+
+}
+
   @override
   Widget build(BuildContext context) {
+    AppBar appBarSearch = AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.red,
+      title: TextField(
+        decoration: const InputDecoration(
+          label: Text("Название"),
+        ),
+        controller: searchController,
+        onChanged: onItemSearch,
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {
+              setState(() {
+                searchController.clear();
+                tittleAppBar = false;
+              });
+            },
+            icon: const Icon(Icons.close))
+      ],
+    );
+    AppBar appBar = AppBar(
+      title: Text(title),
+      centerTitle: true,
+      backgroundColor: Colors.red,
+      actions: [
+        IconButton(
+            onPressed: () {
+              setState(() {
+                tittleAppBar = true;
+              });
+            },
+            icon: const Icon(Icons.search))
+      ],
+    );
+    
     return Scaffold(
+       appBar: tittleAppBar ? appBarSearch : appBar,
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection("deal").snapshots(),
         builder: (context, AsyncSnapshot<dynamic> snapShots) {
@@ -30,11 +79,118 @@ class _dbPageState extends State<dbPage> {
             return ListView.builder(
               physics: const BouncingScrollPhysics(),
               itemCount: snapShots.data.docs.length,
-              itemBuilder: (context, index) => buildList(
-                context,
-                snapShots.data.docs[index],
-                index,
-              ),
+              itemBuilder: (context, index) {
+                String str = snapShots.data?.docs[index].get('title');
+                if (tittleAppBar){
+                  if (str.toLowerCase().contains(searchController.text.toLowerCase())){ 
+                    return Card(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15),
+      side: const BorderSide(
+        color: Colors.black,
+      ),
+    ),
+    child: ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      tileColor: Colors.blueGrey[100],
+      leading: Text((index + 1).toString()),
+      title: Text(snapShots.data?.docs[index].get('title')),
+      subtitle: Text(snapShots.data?.docs[index].get('discription')),
+      trailing: const Icon(
+        Icons.arrow_right,
+        color: Colors.black,
+      ),
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+                  title: const Text("Выберите действие:"),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () async {
+                          CollectionReference deal =
+                              FirebaseFirestore.instance.collection('deal');
+                          await deal.doc(docs.id).delete();
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Удалить")),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => AddDeal(
+                                      title: snapShots.data?.docs[index]['title'],
+                                      discription: snapShots.data?.docs[index]['discription'],
+                                      image: snapShots.data?.docs[index]['image'],
+                                      documentFirebase: snapShots.data?.docs[index],
+                                    )));
+                      },
+                      child: const Text("Редактировать"),
+                    )
+                  ],
+                ));
+      },
+    ),
+  );
+                  }
+                }else{
+                   return Card(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15),
+      side: const BorderSide(
+        color: Colors.black,
+      ),
+    ),
+    child: ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      tileColor: Colors.blueGrey[100],
+      leading: Text((index + 1).toString()),
+      title: Text(snapShots.data?.docs[index].get('title')),
+      subtitle: Text(snapShots.data?.docs[index].get('discription')),
+      trailing: const Icon(
+        Icons.arrow_right,
+        color: Colors.black,
+      ),
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+                  title: const Text("Выберите действие:"),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () async {
+                          CollectionReference deal =
+                              FirebaseFirestore.instance.collection('deal');
+                          await deal.doc(snapShots.data?.docs.id).delete();
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Удалить")),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => AddDeal(
+                                      title: snapShots.data?.docs[index]['title'],
+                                      discription: snapShots.data?.docs[index]['discription'],
+                                      image: snapShots.data?.docs[index]['image'],
+                                      documentFirebase: snapShots.data?.docs[index],
+                                    )));
+                      },
+                      child: const Text("Редактировать"),
+                    )
+                  ],
+                ));
+      },
+    ),
+  );
+                }
+              },
             );
           }
         },
